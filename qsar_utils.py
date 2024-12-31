c9cb2c93be1ae08766f1f4faee8cd49ec76fe7ca
Berne Edmond <be203133@bioinfo>
2024-12-31 15:04:56 +0100

    définitionn du domaine d'applicabilité des données

--- qsar_utils.py
+++ qsar_utils.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-
+def q2_score(y_true, y_pred):
+    """
+    Calcule le Q² = 1 - SSE/SST
+    où SSE = sum of squared errors,
+        SST = sum of squared differences from the mean.
+    """
+    sse = np.sum((y_pred - y_true)**2)
+    sst = np.sum((y_true - np.mean(y_true))**2)
+    return 1 - sse/sst if sst != 0 else 0
 
 def scatter_QSAR(x_data : list[float], 
                  y_data : list[float], 
@@ -29,4 +38,41 @@ def scatter_QSAR(x_data : list[float],
     plt.title(f"Droite de régression depuis un modèle {model_type} entre \nla LC50 prédite et l'expérimentale", fontsize = 15)
     plt.tick_params(labelsize = 12)
     plt.text(0.05, 0.95, f"r = {corr_coef:.2f}", color = "red", fontsize = 14, ha = "left", va = "center", transform = plt.gca().transAxes ) # transform sert à indiquer que les coordonnées x et y sont relatives et non absolues
+    plt.show()
+
+
+def lineplot(x_data : list[float],
+             y_data : list[float],
+             title : str,
+             x_label : str,
+             y_label : str,
+             line_color : str = "black",
+             line_style : str = "solid") -> None:
+    """
+    Fonction qui génère un graphique linéaire
+    """
+    sns.set_style("whitegrid")
+    plt.figure(figsize=(10, 10))
+    sns.lineplot(x = x_data, y = y_data, color = line_color, linestyle = line_style)
+    plt.xlabel(x_label, fontsize = 14)
+    plt.ylabel(y_label, fontsize = 14)
+    plt.title(title, fontsize = 15)
+    plt.tick_params(labelsize = 12)
+    plt.show()
+
+def AD_graph(thresholds : np.ndarray, q2_per_threshold : np.ndarray, pct_out_of_AD : np.ndarray,
+             strict_threshold : float = 0, soft_threshold : float = 0, title : str = ""):
+    """
+    Cette fonction permet de visualiser les espaces optimaux pour le domaine d'applicabilité du modèle QSAR
+    """
+    sns.set_style("whitegrid")
+    sns.lineplot(x=thresholds, y=q2_per_threshold, label="Q²", color = "black" , linestyle = ":")
+    sns.lineplot(x=thresholds, y=pct_out_of_AD, label="% molécules hors AD", color = "black", linestyle = "-")
+    plt.axvline(x=strict_threshold, color='r', linestyle='--', label="Seuil strict")
+    plt.axvline(x=soft_threshold, color='b', linestyle='--', label="Seuil souple")
+    plt.xlabel("Seuil de distance")
+    plt.ylabel("Q² / % molécules hors AD")
+    plt.ylim(0, 1.1)
+    plt.legend()
+    plt.title(title)
     plt.show()
